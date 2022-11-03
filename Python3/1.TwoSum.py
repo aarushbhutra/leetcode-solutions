322a938774bdffe76a6f6706486d064ba87127f0
Aarush  Bhutra <67967009+aarushbhutra@users.noreply.github.com>
2022-11-03 18:47:26 +0545

    Added 3 solutions
    Added 1,9 and 13

---
+++ Python3/1.TwoSum.py
@@ -0,0 +1,23 @@
+class Solution:
+    
+    from typing import List
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        result = []
+        
+        index_map = {}
+        
+        for i, n in enumerate(nums):
+            
+            difference = target - n
+            
+            if difference in index_map:
+                result.append(i)
+                result.append(index_map[difference])
+                break
+            else:
+                index_map[n] = i
+        return result
+                
+        
